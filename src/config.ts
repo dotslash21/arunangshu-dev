b710785de1c336d279a6a98d8806d4e4efdea7a3
Arunangshu Biswas <arunangshubsws@gmail.com>
2023-07-09 22:09:56 +0530

    feat: basic scaffolding done

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Arunangshu Biswas | Software Engineer';
+export const SITE_DESCRIPTION = 'A software-engineer with a graduate degree and a passion for crafting solutions that adds value to people\'s lives.';
+export const SITE_AUTHOR = 'Arunangshu Biswas';
+export const SITE_PREVIEW_IMAGE = '/preview-image.webp';
