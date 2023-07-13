26fbd011a6913960285c6208a0d2dafa4b55c15e
Arunangshu Biswas <arunangshubsws@gmail.com>
2023-07-13 21:36:33 +0530

    feat: added sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://arunangshu.dev',
   integrations: [
     tailwind(),
     image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    })
+      serviceEntryPoint: '@astrojs/image/sharp'
+    }),
+    sitemap()
   ]
 });
